1df60f628e86d9c49c0476358f12bbd35e3d6d77
pandom <pandom@users.noreply.github.com>
2021-11-17 14:16:12 +1100

    deploy new VM

--- main.tf
+++ main.tf
@@ -1,1 +1,49 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+    hcp = {
+        source = "hashicorp/hcp"
+    }
+  }
+}
+provider "hcp" {}
 
+provider "aws" {
+  # Configuration options
+  # Adds Default Tags
+  default_tags {
+    tags = {
+        owner = "burkey@hashicorp.com"
+        name = "burkey"
+        ttl = "72"
+    }
+  }
+  region = var.region
+}
+
+# data "terraform_remote_state" "tf_aws_core" {
+#   backend = "remote"
+
+#   config = {
+#     organization = "burkey"
+#     workspaces = {
+#       name = "tf-aws-core"
+#     }
+#   }
+# }
+
+locals {
+#   public_subnets = data.terraform_remote_state.tf_aws_core.outputs.public_subnets
+#   vpc_id = data.terraform_remote_state.tf_aws_core.outputs.vpc_id
+# 	tags = merge(var.tags, {
+#          owner       = "burkey"
+# 		 se-region   = "apj"
+# 		 purpose     = "customer-demo"
+#          ttl         = "48"
+# 		 terraform   = true
+# 		 hc-internet-facing = true
+#    })
+}
